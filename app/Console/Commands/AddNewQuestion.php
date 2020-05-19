8a9349335448b8c5e726ff5b1952579337b847a1
Mina Nabil Sami <mina.nsami@gmail.com>
2020-05-19 18:48:25 +0200

    Add new command to add new question

---
+++ app/Console/Commands/AddNewQuestion.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Console\ConsoleStringFormatter;
+use App\Console\MenuCommand;
+use App\Services\AddNewQuestion\AddNewQuestionCommand;
+use App\Services\AddNewQuestion\AddNewQuestionHandler;
+
+class AddNewQuestion extends MenuCommand
+{
+    use ConsoleStringFormatter;
+
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'qanda:add-new-question';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Runs an interactive command line to add a new question and its answer.';
+
+    /** @var AddNewQuestionHandler */
+    private $addNewQuestionHandler;
+
+    /**
+     * Create a new command instance.
+     *
+     * @param AddNewQuestionHandler $addNewQuestionHandler
+     */
+    public function __construct(
+        AddNewQuestionHandler $addNewQuestionHandler
+    ) {
+        parent::__construct();
+        $this->addNewQuestionHandler = $addNewQuestionHandler;
+        $this->running = true;
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $this->showMenu();
+
+        while ($this->running) {
+
+            $question = $this->promptQuestion();
+            $answer = $this->promptQuestionAnswer($question);
+
+            if ($this->shouldCancel($question) || $this->shouldCancel($answer)) {
+                return self::CANCEL_EXIT_CODE;
+            }
+
+            if ($this->shouldQuit($question) || $this->shouldQuit($answer)) {
+                $this->quit();
+                return self::TERMINATE_EXIT_CODE;
+            }
+
+            $this->addNewQuestionHandler->handle(
+                new AddNewQuestionCommand($question, $answer)
+            );
+        }
+
+        return self::SUCCESS_EXIT_CODE;
+    }
+
+    /**
+     * * Prompt to ask user for the question's answer
+     *
+     * @param string $question
+     * @return string
+     */
+    private function promptQuestionAnswer(string $question): string
+    {
+        do {
+            $answer = trim($this->ask("Add the model answer for question {$question}"));
+        } while ($answer === '');
+
+        return $answer;
+    }
+
+    /**
+     * Prompt to ask user for question body
+     *
+     * @return string
+     */
+    private function promptQuestion(): string
+    {
+        do {
+            $question = trim($this->ask("Enter the question"));
+        } while ($question === '');
+
+        return $question;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    protected function showMenu(): void
+    {
+        $this->line(
+            $this->writePaddedStringWithLeftRightBorders('-', '-')
+        );
+        $this->line(
+            $this->writePaddedStringWithLeftRightBorders(' ', ' ')
+        );
+        $this->line(
+            $this->writePaddedStringWithLeftRightBorders('Add New Question', ' ')
+        );
+        $this->line(
+            $this->writePaddedStringWithLeftRightBorders(' ', ' ')
+        );
+        $this->line(
+            $this->writePaddedStringWithLeftRightBorders('-', '-')
+        );
+    }
+}
