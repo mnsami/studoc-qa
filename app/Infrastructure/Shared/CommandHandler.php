f58d4ba4d349aa44db1ffa3d6b850872367f9fc1
Mina Nabil Sami <mina.nsami@gmail.com>
2020-05-19 16:30:49 +0200

    add command handler services

---
+++ app/Infrastructure/Shared/CommandHandler.php
@@ -0,0 +1,28 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Infrastructure\Shared;
+
+interface CommandHandler
+{
+    /**
+     * Command class name
+     *
+     * @return string
+     */
+    public function handles(): string;
+
+    /**
+     * Handle command business logic
+     *
+     * @param Command $command
+     * @return DataTransformer
+     */
+    public function handle(Command $command): DataTransformer;
+
+    /**
+     * @param Command $command
+     * @throws SorryWrongCommand
+     */
+    public function assertItHandlesCommand(Command $command);
+}
