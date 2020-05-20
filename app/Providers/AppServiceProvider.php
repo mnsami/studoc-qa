772a0be89d81c416e26baec2484a63c67fecc520
Mina Nabil Sami <mina.nsami@gmail.com>
2020-05-20 22:41:39 +0200

    right place for repository binding

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,10 @@
 
 namespace App\Providers;
 
+use App\Domain\Question\Model\AnswerRepository;
+use App\Domain\Question\Model\QuestionRepository;
+use App\Infrastructure\Persistence\EloquentAnswerRepository;
+use App\Infrastructure\Persistence\EloquentQuestionRepository;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +27,18 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->bind(
+            QuestionRepository::class,
+            function (): QuestionRepository {
+                return new EloquentQuestionRepository();
+            }
+        );
+
+        $this->app->bind(
+            AnswerRepository::class,
+            function (): AnswerRepository {
+                return new EloquentAnswerRepository();
+            }
+        );
     }
 }
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -41,16 +41,6 @@ $app->singleton(
     App\Exceptions\Handler::class
 );
 
-$app->bind(
-    \App\Domain\Question\Model\QuestionRepository::class,
-    \App\Infrastructure\Persistence\EloquentQuestionRepository::class
-);
-
-$app->bind(
-    \App\Domain\Question\Model\AnswerRepository::class,
-    \App\Infrastructure\Persistence\EloquentAnswerRepository::class
-);
-
 /*
 |--------------------------------------------------------------------------
 | Return The Application
